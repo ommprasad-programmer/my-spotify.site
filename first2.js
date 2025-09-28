0b6d119a286920b8b7f5ee11cdbacd31787554f9
Omm Prasad Nath <nathommprasad@gmail.com>
2025-09-28 20:00:56 +0530

    Add files via upload

---
+++ first2.js
@@ -0,0 +1,18 @@
+
+function scrollToLeft() {
+  document.getElementById("cardsList").scrollBy({
+    left: -250,
+    behavior: "smooth"
+  });
+}
+
+function scrollToRight() {
+  document.getElementById("cardsList").scrollBy({
+    left: 250,
+    behavior: "smooth"
+  });
+}
+
+
+
+
